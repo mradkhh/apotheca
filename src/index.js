02760738570c5aaee689d8a0eaeb63f3661cf012
Adkhambek <adkhambekyusupov@gmail.com>
2022-04-29 16:46:16 +0500

    crate

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './Fonts/stylesheet.css';
-import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './Fonts/stylesheet.css';
 import './i18n';
+import './index.css';
 import store from './Redux/store';
-
 
 ReactDOM.render(
   <Provider store={store}>
     <I18nextProvider>
       <BrowserRouter>
         <React.StrictMode>
-         <App />
-       </React.StrictMode>
+          <App />
+        </React.StrictMode>
       </BrowserRouter>
     </I18nextProvider>
-  </Provider>
-  ,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
